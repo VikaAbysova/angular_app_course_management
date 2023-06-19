f9c55fe236c7ed344579ac7c942ff5a0fe4c0526
Vika Abysova <s.vika87@ukr.net>
2023-06-19 18:02:59 +0300

    ht8

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -7,21 +7,17 @@ import {
 import { Observable } from 'rxjs';
 
 export class AuthInterceptor implements HttpInterceptor {
-
-
   intercept(
     req: HttpRequest<object>,
     next: HttpHandler
   ): Observable<HttpEvent<object>> {
     const token = localStorage.getItem('token');
-    if(token){
-
+    if (token) {
       const reqClone = req.clone({
         headers: req.headers.append('Authorization', token),
       });
       return next.handle(reqClone);
     }
-
     return next.handle(req);
   }
 }
